d703ceeda07da73225251cf49dc474a517840db0
rbdoggithub <linuxacademy@gmx.com>
2018-07-11 14:00:48 -0700

    change curl path

--- eol_scalyr/recipes/install_agent.rb
+++ eol_scalyr/recipes/install_agent.rb
@@ -1,7 +1,8 @@
 bash 'scalyr_agent_install' do
         code <<-EOH
 cd /tmp/
-/bin/curl -s -O https://www.scalyr.com/scalyr-repo/stable/latest/scalyr-repo-bootstrap-1.2.1-1.noarch.rpm
+#/bin/curl -s -O https://www.scalyr.com/scalyr-repo/stable/latest/scalyr-repo-bootstrap-1.2.1-1.noarch.rpm
+/usr/bin/curl -s -O https://www.scalyr.com/scalyr-repo/stable/latest/scalyr-repo-bootstrap-1.2.1-1.noarch.rpm
 yum remove -y scalyr-repo scalyr-repo-bootstrap
 yum install -y --nogpgcheck /tmp/scalyr-repo-bootstrap-1.2.1-1.noarch.rpm
 yum install -y scalyr-repo
